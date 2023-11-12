429b47f0ad2d5ab2fb84b3bbea14274d1ea0b9a6
Greg <antonioimportant@gmail.com>
2023-11-12 12:02:20 -0800

    testing some stuff

---
+++ resources/views/site/contact.blade.php
@@ -0,0 +1,1 @@
+<h1>View de contato</h1>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/contato', function () {
+//     return view('site.contact');
+// });
+
+Route::get('/contato', [
+    SiteController::class,
+    'action',
+]);
